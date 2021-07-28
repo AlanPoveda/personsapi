e142ecbe3fbd1fb31a35f2635b4f1a83bf3dd210
Alan Poveda <alsepoveda@gmail.com>
2021-07-28 14:12:49 -0300

    Developed update by id feature

--- src/main/java/com/portifolio/personsapi/controller/PersonController.java
+++ src/main/java/com/portifolio/personsapi/controller/PersonController.java
@@ -52,5 +52,12 @@ public class PersonController {
         personService.delete(id);
     }
 
+    //UPDATE------------------------------------------------------------------------------------------------------------
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+
 
 }
